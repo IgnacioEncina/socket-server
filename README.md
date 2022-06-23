1b675c2ff11c9168d1de4168f703b991a952d4a8
Ignacio <damus@Damus-PC>
2022-06-23 14:28:10 -0400

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+# Socket-Server
+
+
+Reconstruir modulos de Node
+```
+npm install
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar servidor, cualquiera de estos 2 comandos
+```
+nodemosn dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,28 @@
+import router from './routes/router';
+import Server from "./classes/server";
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+// BodyParser   -  Video 16         (ojo va antes de las rutas)
+    // serializa y genera un objeto javascript(json) para usar en servicios express
+server.app.use( bodyParser.urlencoded({ extended: true }));
+    // pasa la informacion de un formato json
+server.app.use( bodyParser.json() );
+
+
+// Video 17 - COnfiguracion del CORS
+server.app.use( cors({ origin: true, credentials: true }))
+
+
+ 
+server.app.use('/', router);
+
+
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+});
